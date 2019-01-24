964f2b3e8e968838e725908627bdcda25102d6df
Rayleigh <gmj1197@gmail.com>
2019-01-24 18:53:56 +0900

    feat: Add deepspeech using the buffer data

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+import dsb from "./dsBuffer";
 import dsf from "./dsFile";
+
+function dsBuffer(buffer) {
+  if (!buffer) return 1;
+  dsb(buffer);
+}
 
 function dsFile(filepath) {
   if (!filepath) return 1;
@@ -6,5 +12,6 @@ function dsFile(filepath) {
 }
 
 module.exports = {
+  dsBuffer: dsBuffer,
   dsFile: dsFile
 };
