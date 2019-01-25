b04ad1f5b51c2602082ad83037840adc4e299daf
Rayleigh <gmj1197@gmail.com>
2019-01-25 13:35:04 +0900

    fix: Modify file-demo contents

--- file-demo.js
+++ file-demo.js
@@ -3,5 +3,5 @@ const deepspeech = require("./index");
 // // using a sample file
 // deepspeech.dsFile();
 
-// input your file path
+// input your file path here
 deepspeech.dsFile("./audio/8455-210777-0068.wav");
